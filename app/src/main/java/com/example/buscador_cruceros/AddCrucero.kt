1c05f40d838165348418134140a5ee36e54cecab
Pablo  Martin <pablomartinsanchezseco02@gmail.com>
2023-10-15 22:42:50 +0200

    Create Cruise Finish

--- app/src/main/java/com/example/buscador_cruceros/AddCrucero.kt
+++ app/src/main/java/com/example/buscador_cruceros/AddCrucero.kt
@@ -1,9 +1,13 @@
 package com.example.buscador_cruceros
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
+import android.graphics.Typeface
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,12 +18,14 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.NumberPicker
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia.*
+import androidx.core.content.ContextCompat
 import com.example.buscador_cruceros.Models.Crucero
 import com.example.buscador_cruceros.Models.Naviera
 import com.google.firebase.firestore.ktx.firestore
@@ -33,19 +39,23 @@ class AddCrucero : AppCompatActivity() {
 
     lateinit var sharedPreferences: SharedPreferences
     lateinit var editorPreferences: SharedPreferences.Editor
-    /*var sharedPreferences: SharedPreferences = getSharedPreferences("ImgPreferences", Context.MODE_PRIVATE)
-    var editorPreferences: SharedPreferences.Editor = sharedPreferences.edit()*/
 
     var listNavieras = mutableListOf<String>()
+    var yearConstruction: String = ""
 
     lateinit var etNameCruise: TextView
     lateinit var spNavieras: Spinner
+    lateinit var tvYearConstruction: TextView
     lateinit var etTonelaje: TextView
     lateinit var etPasajeros: TextView
     lateinit var etTripulantes: TextView
     lateinit var imgUpload: ImageView
     lateinit var etDescripcion: EditText
     lateinit var btnAdd: Button
+
+    lateinit var yearPicker: NumberPicker
+    lateinit var btnYearConfirm: Button
+    lateinit var imgCalendar: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,12 +67,14 @@ class AddCrucero : AppCompatActivity() {
     fun initComponent(){
         etNameCruise = findViewById(R.id.etNameCruise)
         spNavieras = findViewById(R.id.spNavieras)
+        tvYearConstruction = findViewById(R.id.tvYearConstruction)
         etTonelaje = findViewById(R.id.etTonelaje)
         etPasajeros = findViewById(R.id.etPasajeros)
         etTripulantes = findViewById(R.id.etTripulantes)
         etDescripcion = findViewById(R.id.etDescripcion)
         imgUpload = findViewById(R.id.imgUpload)
         btnAdd = findViewById(R.id.btnAdd)
+        imgCalendar = findViewById(R.id.imgCalendar)
 
         var adapterSpinner = ArrayAdapter<String>(this, com.bumptech.glide.R.layout.support_simple_spinner_dropdown_item, listNavieras)
         spNavieras.adapter = adapterSpinner
@@ -74,6 +86,7 @@ class AddCrucero : AppCompatActivity() {
         getAllNavieras()
         uploadImgStorage()
         getCompany()
+        fragmentYear()
     }
 
     fun createSharedPreferences(){
@@ -133,9 +146,15 @@ class AddCrucero : AppCompatActivity() {
             }
 
             override fun onNothingSelected(parentView: AdapterView<*>?) {
-                // Maneja el caso en el que no se ha seleccionado nada
+                Toast.makeText(spNavieras.context, getString(R.string.mensaje_Naviera), Toast.LENGTH_LONG).show()
             }
         }
+    }
+
+    fun createInfoDescripcion(descripcion: String): String {
+        var listDescripcion = descripcion.split(".")
+        var infoDescripcion = listDescripcion[0]+"."
+        return infoDescripcion
     }
 
     fun addCruise(idNaviera: String){
@@ -143,26 +162,64 @@ class AddCrucero : AppCompatActivity() {
             var nameNaviera = idNaviera
             var getUriReferences = Uri.parse(sharedPreferences.getString("imgURI", null))
             var idDocument = db.collection("crucero").document().id
-            var crucero = Crucero(idDocument, etNameCruise.text.toString(), etDescripcion.text.toString(), etDescripcion.text.toString(), nameNaviera, etPasajeros.text.toString().toInt(), "2020", idDocument, etTripulantes.text.toString().toInt(), etTonelaje.text.toString().toInt())
-            var imgReference = storageRef.child("cruceros/${idDocument}.png")
+            var nameCrucero = etNameCruise.text.toString()
+            var descripcion = etDescripcion.text.toString()
+            var infoDescripcion = createInfoDescripcion(etDescripcion.text.toString())
+            var pasajeros = etPasajeros.text.toString().toInt()
+            var tripulantes = etTripulantes.text.toString().toInt()
+            var tonelaje = etTonelaje.text.toString().toInt()
 
-            db.collection("crucero")
-                .document(idDocument)
-                .set(crucero)
-                .addOnSuccessListener {
-                    imgReference.putFile(getUriReferences)
-                        .addOnSuccessListener {
-                            Log.i("Result", "Okey Upload Image")
-                            var intent = Intent(this, BuscadorCrucero::class.java)
-                            startActivity(intent)
-                        }
-                        .addOnFailureListener{ e ->
-                            Log.i("Result", "Error Upload Image")
-                        }
-                }
-                .addOnFailureListener {
+            if(idDocument.isNotEmpty() && nameCrucero.isNotEmpty() && descripcion.isNotEmpty() && infoDescripcion.isNotEmpty() && nameNaviera.isNotEmpty() && pasajeros != null && idDocument.isNotEmpty() && tripulantes != null && tonelaje != null && yearConstruction != ""){
+                var crucero = Crucero(idDocument, nameCrucero, descripcion, infoDescripcion, nameNaviera, pasajeros, yearConstruction, idDocument, tripulantes, etTonelaje.text.toString().toInt())
+                var imgReference = storageRef.child("cruceros/${idDocument}.png")
 
-                }
+                db.collection("crucero")
+                    .document(idDocument)
+                    .set(crucero)
+                    .addOnSuccessListener {
+                        imgReference.putFile(getUriReferences)
+                            .addOnSuccessListener {
+                                Log.i("Result", "Okey Upload Image")
+                                var intent = Intent(this, BuscadorCrucero::class.java)
+                                startActivity(intent)
+                            }
+                            .addOnFailureListener{ e ->
+                                Log.i("Result", "Error Upload Image")
+                            }
+                    }
+                    .addOnFailureListener {
+
+                    }
+            }else{
+                Toast.makeText(this, "Debes de rellenar todos los campos", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    @SuppressLint("NewApi")
+    fun fragmentYear(){
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_year)
+
+        yearPicker = dialog.findViewById(R.id.yearPicker)
+        btnYearConfirm = dialog.findViewById(R.id.btnYearConfirm)
+        yearPicker.textColor = ContextCompat.getColor(this, R.color.white)
+        yearPicker.minValue = 1900
+        yearPicker.maxValue = 2030
+        yearPicker.value = 2023
+
+        yearPicker.setOnValueChangedListener { _, _, newVal ->
+            yearConstruction = newVal.toString()
+        }
+
+        imgCalendar.setOnClickListener {
+            dialog.show()
+        }
+
+        btnYearConfirm.setOnClickListener {
+            dialog.hide()
+            tvYearConstruction.text = getString(R.string.text_año)+": ${yearConstruction}"
+            tvYearConstruction.setTypeface(null, Typeface.NORMAL)
         }
     }
 
--- app/src/main/java/com/example/buscador_cruceros/BuscadorCrucero.kt
+++ app/src/main/java/com/example/buscador_cruceros/BuscadorCrucero.kt
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SearchView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.buscador_cruceros.Adapter.CruceroAdapter
 import com.example.buscador_cruceros.Models.Crucero
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 
@@ -49,6 +52,7 @@ class BuscadorCrucero : AppCompatActivity() {
         rvCrucero.adapter = cruceroAdapter
         getAll()
         goAddCruise()
+        funcionalitySearchView()
     }
 
     fun getAll(){
@@ -67,6 +71,40 @@ class BuscadorCrucero : AppCompatActivity() {
         btnFloating.setOnClickListener {
             var intent = Intent(this, AddCrucero::class.java)
             startActivity(intent)
+        }
+    }
+
+    fun funcionalitySearchView(){
+        svCrucero.setOnQueryTextListener( object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                filterList(query)
+                return false
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                return false
+            }
+
+        })
+    }
+
+    fun filterList(query: String?){
+        if(query != null){
+            listCruceros.clear()
+            db.collection("crucero")
+                .get()
+                .addOnSuccessListener {
+                    for (item in it){
+                        var crucero = item.toObject<Crucero>(Crucero::class.java)
+                        if(crucero.name.toString().toLowerCase().contains(query)){
+                            listCruceros.add(crucero)
+                        }
+                    }
+                    actualizarAdapter()
+                }
+                .addOnFailureListener {
+
+                }
         }
     }
 
